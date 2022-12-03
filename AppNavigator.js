7dafc0d9f87bc63a64af52b790020cef1a2800c8
info3170sbartnicki <s_bartnicki@fanshaweonline.ca>
2022-12-03 16:11:52 -0500

    screen styled

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,10 +1,10 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
-import { Platform } from "react-native";
+import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
+import { Platform } from 'react-native';
 
-import AuthScreen from "./screens/AuthScreen";
-import NotesListScreen from "./screens/NotesListScreen";
-import NoteDetailsScreen from "./screens/NoteDetailsScreen";
+import AuthScreen from './screens/AuthScreen';
+import NotesListScreen from './screens/NotesListScreen';
+import NoteDetailsScreen from './screens/NoteDetailsScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,42 +13,41 @@ navTheme.colors.background = '#fff';
 
 function AppNavigator() {
   return (
-      <NavigationContainer
-        theme={navTheme}
+    <NavigationContainer theme={navTheme}>
+      <Stack.Navigator
+        screenOptions={{
+          headerMode: 'screen',
+          headerTintColor: Platform.OS === 'android' ? 'white' : 'blue',
+          headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? 'white' : 'white',
+          },
+        }}
       >
-          <Stack.Navigator
-            screenOptions={{
-                headerMode: 'screen',
-                headerTintColor: Platform.OS === 'android' ? 'white' : 'blue',
-                headerStyle: {
-                    backgroundColor: Platform.OS === 'android' ? 'white' : 'white',
-                }
-            }}
-          >
-              <Stack.Screen
-                  name="Log In Page"
-                  component={ AuthScreen }
-                  options={{
-                      title: '',
-                  }}
-              />
-              <Stack.Screen
-                  name="Notes List"
-                  component={ NotesListScreen }
-                  options={{
-                      title: '',
-                  }}
-              />
-              <Stack.Screen
-                  name="Note Details"
-                  component={ NoteDetailsScreen }
-                  options={{
-                      title: '',
-                  }}
-              />
-          </Stack.Navigator>
-      </NavigationContainer>
-  )
+        <Stack.Screen
+          name="Log In Page"
+          component={AuthScreen}
+          options={{
+            title: '',
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="Notes List"
+          component={NotesListScreen}
+          options={{
+            title: '',
+          }}
+        />
+        <Stack.Screen
+          name="Note Details"
+          component={NoteDetailsScreen}
+          options={{
+            title: '',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default AppNavigator;
