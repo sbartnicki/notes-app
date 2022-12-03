9cb81ab1a6be7c26751ec6bc47d717595c87afbf
Sly <sylwesterbartnicki@gmail.com>
2022-12-03 13:44:48 -0500

    Testing git

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,21 +1,19 @@
-import { View, Text, Pressable } from "react-native";
+import { View, Text, Pressable } from 'react-native';
+
+// Test
 
 const AuthScreen = (props) => {
-    return (
-        <View>
-            <Text>Auth Page</Text>
-            <Pressable
-                onPress={() => props.navigation.navigate('Note Details')}
-            >
-                <Text>Go to Note Details</Text>
-            </Pressable>
-            <Pressable
-                onPress={() => props.navigation.navigate('Notes List')}
-            >
-                <Text>Go to Notes List page</Text>
-            </Pressable>
-        </View>
-    )
-}
+  return (
+    <View>
+      <Text>Auth Page</Text>
+      <Pressable onPress={() => props.navigation.navigate('Note Details')}>
+        <Text>Go to Note Details</Text>
+      </Pressable>
+      <Pressable onPress={() => props.navigation.navigate('Notes List')}>
+        <Text>Go to Notes List page</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 export default AuthScreen;
